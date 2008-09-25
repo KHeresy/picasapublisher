7e5c1ea9596577d9dc0562e5cbcf1b5cfbbe55b0
mlsteeves <mlsteeves@5d9781e0-8b07-11dd-8937-b3a01dae0fbe>
2008-09-25 14:07:54 +0000

    git-svn-id: http://picasapublisher.googlecode.com/svn/trunk@2 5d9781e0-8b07-11dd-8937-b3a01dae0fbe

---
+++  picasapublisher/PicasaUpload/PicasaPublisher.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GoogleApi;
+using System.Xml;
+
+namespace PicasaUpload
+{
+	public class PicasaPublisher : Microsoft.WindowsLive.PublishPlugins.IPublishPlugin
+	{
+		/*
+		 * Following: http://code.google.com/apis/picasaweb/developers_guide_protocol.html#Auth
+		 * http://msdn.microsoft.com/en-us/library/cc967073.aspx
+		 */
+		#region IPublishPlugin Members
+
+		public bool HasPublishResults(System.Xml.XmlDocument sessionXml)
+		{
+			return false;
+		}
+
+		public bool HasSummaryInformation(System.Xml.XmlDocument sessionXml)
+		{
+			return false;
+		}
+
+		public void LaunchPublishResults(System.Xml.XmlDocument sessionXml)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool PublishItem(System.Windows.Forms.IWin32Window parentWindow, string mediaObjectId, System.IO.Stream stream, System.Xml.XmlDocument sessionXml, Microsoft.WindowsLive.PublishPlugins.IPublishProperties publishProperties, Microsoft.WindowsLive.PublishPlugins.IPublishProgressCallback callback, System.Threading.EventWaitHandle cancelEvent)
+		{
+			//get our filename out of the sessionXml
+			XmlNode itemPublishing = sessionXml.SelectSingleNode(string.Format("//PhotoGalleryPublishSession/ItemSet/Item[@id=\"{0}\"]",mediaObjectId));
+
+			string filename = itemPublishing["OriginalFileName"].InnerText;
+
+			//get our information out of session:
+			XmlElement googleSettings = sessionXml.DocumentElement["GoogleSettings"];
+			string googleAuthKey = googleSettings["AuthKey"].InnerText;
+			string albumId = googleSettings["AlbumId"].InnerText;
+
+			return GoogleApi.PicasaWebAlbums.PicasaWebAlbumsRequest.PostPhotoWithoutMetadata(albumId, stream, filename, new GoogleApi.GoogleAuthorizationToken(googleAuthKey));
+
+		}
+
+		public bool ShowConfigurationSettings(System.Windows.Forms.IWin32Window parentWindow, System.Xml.XmlDocument sessionXml, System.Xml.XmlDocument persistXml, Microsoft.WindowsLive.PublishPlugins.IPublishProperties publishProperties)
+		{
+			try
+			{
+				PicasaPubliserForm form = new PicasaPubliserForm();
+
+				if (form.ShowDialog() == DialogResult.OK)
+				{
+					XmlElement settings = sessionXml["GoogleSettings"];
+					XmlElement authKey = null;
+					XmlElement albumId = null;
+					if (settings == null)
+					{
+						settings = sessionXml.CreateElement("GoogleSettings");
+						authKey = sessionXml.CreateElement("AuthKey");
+						albumId = sessionXml.CreateElement("AlbumId");
+						sessionXml.DocumentElement.AppendChild(settings);
+						settings.AppendChild(authKey);
+						settings.AppendChild(albumId);
+					}
+					else
+					{
+						authKey = settings["AuthKey"];
+						albumId = settings["AlbumId"];
+					}
+
+					authKey.InnerText = form.GoogleAuthKey.AuthorizationToken;
+					albumId.InnerText = form.SelectedAlbumId;
+
+					return true;
+				}
+			}
+			catch (Exception x)
+			{
+				MessageBox.Show(x.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			///*set the settings */
+
+			return false;
+		}
+
+		public void ShowSummaryInformation(System.Windows.Forms.IWin32Window parentWindow, System.Xml.XmlDocument sessionXml)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++  picasapublisher/PicasaUploadTester/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PicasaUploadTester
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void _cmdTestLogin_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			PicasaUpload.PicasaPubliserForm form = new PicasaUpload.PicasaPubliserForm();
+			if (form.ShowDialog() == DialogResult.OK)
+			{
+				//choose file
+				openFileDialog1.Filter = "Image Files|*.jpeg,*.jpg,*.png,*.gif,*.bmp|All Files|*.*";
+
+				if (openFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string filename = openFileDialog1.FileName;
+
+					GoogleApi.PicasaWebAlbums.PicasaWebAlbumsRequest.PostPhotoWithoutMetadata(form.SelectedAlbumId, File.OpenRead(filename), Path.GetFileName(filename), form.GoogleAuthKey);
+				}
+			}
+		}
+	}
+}
