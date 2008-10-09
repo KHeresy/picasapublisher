ba7197c825d2cd15640f3fa537b51fd7cc685529
mlsteeves <mlsteeves@5d9781e0-8b07-11dd-8937-b3a01dae0fbe>
2008-10-09 15:11:18 +0000

    V1.0.1
    git-svn-id: http://picasapublisher.googlecode.com/svn/trunk@60 5d9781e0-8b07-11dd-8937-b3a01dae0fbe

---
+++  picasapublisher_1.0.1/PicasaUploadTester/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PicasaUploadTester
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void _cmdTestLogin_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			PicasaUpload.LoginForm form = new PicasaUpload.LoginForm();
+			if (form.ShowDialog() == DialogResult.OK)
+			{
+				//choose file
+				openFileDialog1.Filter = "Image Files|*.jpeg,*.jpg,*.png,*.gif,*.bmp|All Files|*.*";
+
+				if (openFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string filename = openFileDialog1.FileName;
+
+					GoogleApi.PicasaWebAlbums.PicasaWebAlbumsRequest.PostPhotoWithoutMetadata(form.SelectedAlbumId, File.OpenRead(filename), Path.GetFileName(filename), form.GoogleAuthKey);
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			PicasaUpload.UI.SelectAlbum.SelectAlbumUI();
+		}
+	}
+}
